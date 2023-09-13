e1b2982a68c9ca339a38bd293a6960795d79c1e6
Benjamin <benjamin.valleran@hotmail.fr>
2023-09-13 18:16:55 +0200

    test commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const Titrepage = document.querySelector(".titrepage");
+
+let i = 0;
+
+console.log(Titrepage);
+
+Titrepage.addEventListener("click", () => {
+    i++;
+    console.log(i);
+
+    })
